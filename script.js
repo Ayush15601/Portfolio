21a8fcd45561c93a44c1c1fc8941b9088610a0e5
Ayush15601 <aayushtewarit@gmail.com>
2026-04-23 21:27:15 +0530

    updated resume

--- script.js
+++ script.js
@@ -4,17 +4,17 @@ let navbar = document.querySelector(".navbar");
 let sections = document.querySelectorAll("section");
 let navLinks = document.querySelectorAll("header nav a");
 
-window.onscroll = () => {
+window.onscroll = function () {
     let top = window.scrollY;
 
-    sections.forEach(sec => {
+    sections.forEach(function (sec) {
         let offset = sec.offsetTop - 150;
         let height = sec.offsetHeight;
         let id = sec.getAttribute("id");
 
         if(top >= offset && top < offset + height){
 
-            navLinks.forEach(link => link.classList.remove("active"));
+            navLinks.forEach( function (link) {link.classList.remove("active")} );
 
             let activeLink = document.querySelector(`header nav a[href*="${id}"]`);
             if(activeLink){
